20b0b9cbab158c77870429a11ed8232358c9f809
Sigurdarson <gunnar@sigurdarson.is>
2019-05-23 23:40:07 +0200

    Added more colors + names

--- src/components/Colors/ColorSection.js
+++ src/components/Colors/ColorSection.js
@@ -8,64 +8,76 @@ function ColorSection() {
             <div className="grid">
 
                 <ColorCard 
-                name= "Sky Blue"
+                name= "Ultramarine blue"
                 colorcode= "#4776FF"
                 background= "#4776FF"
                 delay= ".2s"
                 />
                 <ColorCard 
-                name= "Sky Blue"
+                name= "Dull Magenta"
                 colorcode= "#DD3CBE"
                 background= "#DD3CBE"
                 delay= ".4s"
                 />
                 <ColorCard 
-                name= "Sky Blue"
+                name= "Electric Violet"
                 colorcode= "#8C13FC"
                 background= "#8C13FC"
                 delay= ".6s"
                 />
                 <ColorCard 
-                name= "Sky Blue"
+                name= "Mountain Meadow"
                 colorcode= "#25C181"
                 background= "#25C181"
                 delay= ".8s"
                 />
                  <ColorCard 
-                name= "Sky Blue"
+                name= "Fuel Yellow"
                 colorcode= "#F5A623"
                 background= "#F5A623"
                 delay= "1s"
                 />
                 <ColorCard 
-                name= "Sky Blue"
+                name= "Mandy"
                 colorcode= "#EC506E"
                 background= "#EC506E"
                 delay= "1.2s"
                 />
                 <ColorCard 
-                name= "Sky Blue"
+                name= "Shamrock"
                 colorcode= "#54D89F"
                 background= "#54D89F"
                 delay= "1.4s"
                 />
                 <ColorCard 
-                name= "Sky Blue"
+                name= "Picton Blue"
                 colorcode= "#55ACEE"
                 background= "#55ACEE"
                 delay= "1.6s"
                 />
                 <ColorCard 
-                name= "Sky Blue"
+                name= "Fiord"
                 colorcode= "#445166"
                 background= "#445166"
                 delay= "1.8s"
                 />
                 <ColorCard 
-                name= "Sky Blue"
+                name= "Cryon Orange"
                 colorcode= "#FF5D1F"
                 background= "#FF5D1F"
                 delay= "2s"
+                />
+                <ColorCard 
+                name= "Watermelon"
+                colorcode= "#ff4757"
+                background= "#ff4757"
+                delay= "2.2s"
+                />
+                <ColorCard 
+                name= "Baltic Sea"
+                colorcode= "#3d3d3d"
+                background= "#3d3d3d"
+                delay= "2.4s"
                 />
 
             </div>
