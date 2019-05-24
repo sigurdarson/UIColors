95052feb12c51024023fb4f14f57fa96ded1aad4
Sigurdarson <gunnar@sigurdarson.is>
2019-05-24 16:54:02 +0200

    Added Copy to clipboard function

--- src/components/Colors/ColorCard.js
+++ src/components/Colors/ColorCard.js
@@ -1,25 +1,42 @@
 import React from "react";
 import './Color.css';
+import useClippy from 'use-clippy';
+
 
 function ColorCard(props) {
     console.log(props)
+    const [ clipboard, setClipboard ] = useClippy();
     return (
 
-        <article className="colorCard" style={{ animationDelay: props.delay }}>
-        <span className="colorName">
+        <article
+            className="colorCard"
+            style={{ animationDelay: props.delay }}>
+        <span 
+            className="colorName">
             {props.name}
         </span>
-        <div className="color" style={{background: props.background}}>
-
+        <div
+            className="color"
+            style={{background: props.background}}>
         </div>
-        <span className="colorCode">
-        {props.colorcode}
+
+        <span 
+            className="colorCode">
+            {props.colorcode}
         </span>
-        <span className="copyCode">
+        <span
+            className="copyCode">
+            Copy HEX
+        </span>
+        <span
+            className="copyCode"
+            onClick={() => {setClipboard(props.colorcode);}}>
             Copy HEX
         </span>
     </article>
     );
-}
+}; 
+
+
 
 export default ColorCard
